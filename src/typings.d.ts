1aa3b2bdc15cd900f86c83a22b1c79d678edb2ee
Huan LI <zixia@zixia.net>
2018-12-29 22:25:58 +0800

    using official leveldb types (https://github.com/Level/community/issues/16)

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,7 +2,3 @@ declare module '*/package.json' {
   export const version: string
   // export default version
 }
-
-declare module 'leveldown'
-declare module 'encoding-down'
-declare module 'levelup'
