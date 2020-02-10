6869c92a0fc81bac1bddb5a9152dfc67876a2f60
Huan LI <zixia@zixia.net>
2020-02-10 19:19:00 +0800

    use private log

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
 /// <reference path="./typings.d.ts" />
 
+import { Brolog }      from 'brolog'
+
 // https://github.com/Microsoft/TypeScript/issues/14151#issuecomment-280812617
 if (!Symbol.asyncIterator) {
   (<any>Symbol).asyncIterator = Symbol.for('Symbol.asyncIterator')
 }
 
-export { log }      from 'brolog'
+const log = new Brolog()
 
 export { VERSION }  from './version'
+
+export { log }
