5e7eab1227f14be4defd6bab8cf0d64d618e3e6d
Huan LI <zixia@zixia.net>
2020-04-16 08:27:19 +0800

    upgrade rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
-import json from 'rollup-plugin-json'
+import json from '@rollup/plugin-json'
 
 export default {
-  entry: 'dist/index.js',
-  dest: 'bundles/flash-store.es6.umd.js',
-  sourceMap: true,
-  format: 'umd',
-  moduleName: 'window',
-  banner: '/* flash-store version ' + require('./package.json').version + ' */',
-  footer: '/* https://github.com/zixia */',
+  input: 'dist/index.js',
+  output: {
+    file: 'bundles/flash-store.es6.umd.js',
+    name: 'window',
+    sourcemap: true,
+    format: 'umd',
+    banner: '/* flash-store version ' + require('./package.json').version + ' */',
+    footer: '/* https://github.com/huan */',
+  },
   plugins: [
     json({
       // All JSON files will be parsed by default,
       // but you can also specifically include/exclude files
       // include: 'node_modules/**',  // Default: undefined
       // exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-	    preferConst: true, // Default: false
-    })
-  ]
+      preferConst: true, // Default: false
+    }),
+  ],
 }
