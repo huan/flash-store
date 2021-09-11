a58d981b508b2ee085253e79cd8f22ec52e66a4e
Huan LI (李卓桓) <zixia@zixia.net>
2021-09-12 02:28:24 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,17 +1,24 @@
+import os from 'os'
+
 import {
   VERSION,
   FlashStore,
   FlashStoreSync,
 }                   from 'flash-store'
 
-const store = new FlashStore()
-store.destroy()
+async function main () {
+  const store = new FlashStore(os.tmpdir())
+  await store.destroy()
 
-const storeSync = new FlashStoreSync()
-storeSync.destroy()
+  const storeSync = new FlashStoreSync(os.tmpdir())
+  await storeSync.destroy()
 
-if (VERSION === '0.0.0') {
-  throw new Error('version should be set before publishing')
+  if (VERSION === '0.0.0') {
+    throw new Error('version should be set before publishing')
+  }
+
+  console.log(`Smoke Testing v${store.version()} PASSED!`)
 }
 
-console.log(`Smoke Testing v${store.version()} PASSED!`)
+main()
+  .catch(console.error)
